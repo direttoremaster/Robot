74c21e2d3f42c4e2f5be33e6f92260f37c0e1ae5
direttoremaster <direttoremaster@live.it>
2014-07-25 14:06:24 +0200

    Create Gyro.ino

---
+++ Gyro.ino
@@ -0,0 +1,88 @@
+// Source: https://github.com/TKJElectronics/
+
+#include <Wire.h>
+#include "Kalman.h" 
+ 
+ 
+//Get data 
+#include <Wire.h>
+#include "Kalman.h"
+ 
+/* IMU Data */
+int16_t accX, accY, accZ;
+int16_t tempRaw;
+int16_t gyroX, gyroY, gyroZ;
+ 
+Kalman kalmanX; // Create the Kalman instances
+Kalman kalmanY;
+ 
+ 
+double accXangle, accYangle; // Angle calculate using the accelerometer
+double temp; // Temperature
+double gyroXangle, gyroYangle; // Angle calculate using the gyro
+double compAngleX, compAngleY; // Calculate the angle using a complementary filter
+double kalAngleX, kalAngleY; // Calculate the angle using a Kalman filter
+ 
+uint32_t timer;
+uint8_t i2cData[14]; // Buffer for I2C data
+ 
+ 
+void setGyro() {
+ 
+ 
+ 
+  Wire.begin();
+  i2cData[0] = 7; // Set the sample rate to 1000Hz - 8kHz/(7+1) = 1000Hz
+  i2cData[1] = 0x00; // Disable FSYNC and set 260 Hz Acc filtering, 256 Hz Gyro filtering, 8 KHz sampling
+  i2cData[2] = 0x00; // Set Gyro Full Scale Range to ±250deg/s
+  i2cData[3] = 0x00; // Set Accelerometer Full Scale Range to ±2g
+  while(i2cWrite(0x19,i2cData,4,false)); // Write to all four registers at once
+  while(i2cWrite(0x6B,0x01,true)); // PLL with X axis gyroscope reference and disable sleep mode 
+  while(i2cRead(0x75,i2cData,1));
+  if(i2cData[0] != 0x68) { // Read "WHO_AM_I" register
+    Serial.print(F("Errore lettura sensori"));
+    while(1);
+  }
+  delay(100); // Wait for sensor to stabilize
+  
+  while(i2cRead(0x3B,i2cData,6));
+  accX = ((i2cData[0] << 8) | i2cData[1]);
+  accY = ((i2cData[2] << 8) | i2cData[3]);
+  accZ = ((i2cData[4] << 8) | i2cData[5]);
+  // atan2 outputs the value of -π to π (radians) - see http://en.wikipedia.org/wiki/Atan2
+  // We then convert it to 0 to 2π and then from radians to degrees
+  accYangle = (atan2(accX,accZ)+PI)*RAD_TO_DEG;
+  accXangle = (atan2(accY,accZ)+PI)*RAD_TO_DEG;
+  kalmanX.setAngle(accXangle); // Set starting angle
+  kalmanY.setAngle(accYangle);
+  gyroXangle = accXangle;
+  gyroYangle = accYangle;
+  compAngleX = accXangle;
+  compAngleY = accYangle;
+  timer = micros();
+  
+  }
+  
+ 
+double getGyro(){
+while(i2cRead(0x3B,i2cData,14));
+  accX = ((i2cData[0] << 8) | i2cData[1]);
+  accY = ((i2cData[2] << 8) | i2cData[3]);
+  accZ = ((i2cData[4] << 8) | i2cData[5]);
+  tempRaw = ((i2cData[6] << 8) | i2cData[7]);  
+  gyroX = ((i2cData[8] << 8) | i2cData[9]);
+  gyroY = ((i2cData[10] << 8) | i2cData[11]);
+  gyroZ = ((i2cData[12] << 8) | i2cData[13]);
+  
+  accYangle = (atan2(accX,accZ)+PI)*RAD_TO_DEG;
+    
+  double gyroYrate = -((double)gyroY/131.0);
+ 
+  kalAngleY = kalmanY.getAngle(accYangle, gyroYrate, (double)(micros()-timer)/1000000);
+  timer = micros();
+  
+  temp = ((double)tempRaw + 12412.0) / 340.0;
+//if(abs(kalAngleY-setPoint)<0.5)	driveMotor(90); per inserire la zona morta togliere il commento
+  return kalAngleY;
+ 
+}	
